4effa81a12e10ca524c2408700859a1d4a43eaaf
JQM96 <jquintanamartin.games@gmail.com>
2025-11-11 16:06:10 -0300

    Simple qued messages

--- Message System/Assets/DialogManager.cs
+++ Message System/Assets/DialogManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -5,13 +6,31 @@ public class DialogManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI dialogText;
 
+    Queue<string> quedMessages = new Queue<string>();
+
     private void Start()
     {
-        SetText("Very simple implementation...");
+        QueMessage("Very simple implementation...");
+        QueMessage("...");
+        QueMessage("Yes...");
+        QueMessage("Very simple...");
+        QueMessage("Very implementation...");
+        QueMessage("...");
     }
 
     public void SetText(string newText)
     {
         dialogText.text = newText;
+    }
+
+    public void QueMessage(string message)
+    {
+        quedMessages.Enqueue(message);
+    }
+
+    public void ShowQuedMessage()
+    {
+        if (quedMessages.Count > 0)
+            SetText(quedMessages.Dequeue());
     }
 }
